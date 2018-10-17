c267a60c7d597b8ef4d0b485d8facfdb8627a7fc
Jonas Stattin <jonas.i.stattin@gmail.com>
2018-10-17 16:35:40 +0200

    More v4 changes

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
-const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	optimization: {
+		minimizer: [new UglifyJsPlugin()]
 	},
 	module: {
 		rules: [
